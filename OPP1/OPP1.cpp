f3748c90e4682adc562b515c8cd0b98cd1e3f79b
i3bdolah <xbdullahx@gmail.com>
2024-02-23 00:37:35 +0300

    FIrst Class

--- OPP1/OPP1.cpp
+++ OPP1/OPP1.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
+
+class Arabian {
+public:
+    string name;
+    string age;
+    string city;
+
+    string PrintInfo() {
+        return name + " is " + age + " years old. live in " + city + ".";
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Arabian Abdullah;
+    Abdullah.name = "Abdullah Al-Shehri";
+    Abdullah.age = "23";
+    Abdullah.city = "Al-Namas";
+
+    cout << Abdullah.PrintInfo() << endl;
 }
