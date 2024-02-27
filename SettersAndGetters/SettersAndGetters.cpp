f8585905c38af84f6afdf50b039ff39ef07e2b3b
i3bdolah <xbdullahx@gmail.com>
2024-02-28 00:58:26 +0300

    Set & Get

---
+++ SettersAndGetters/SettersAndGetters.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Arabian {
+private:
+	int _id = 102830;
+	string _name;
+	string _age;
+	string _city;
+
+public:
+	int ID() {
+		return _id;
+	}
+
+	void setName(string str) {
+		_name = str;
+	}
+	string Name() {
+		return _name;
+	}
+
+	void setAge(string str) {
+		_age = str;
+	}
+	string Age() {
+		return _age;
+	}
+
+	void setCity(string str) {
+		_city = str;
+	}
+	string City() {
+		return _city;
+	}
+
+	string PrintInfo() {
+		return Name() + " is " + Age() + " years old. live in " + City() + ".";
+	}
+};
+
+int main()
+{
+	Arabian Me;
+	Me.setName("Abdullah");
+	Me.setAge("23");
+	Me.setCity("Jeddah");
+
+	cout << Me.ID() << endl;
+	cout << Me.Name() << endl;
+	cout << Me.Age() << endl;
+	cout << Me.City() << "\n\n";
+
+	cout << Me.PrintInfo() << endl;
+}
