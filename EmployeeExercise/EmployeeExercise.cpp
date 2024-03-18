3a6dd5556eb505efbc04f6aa02186e05f63a7119
i3bdolah <xbdullahx@gmail.com>
2024-03-19 00:18:54 +0300

    Employee Exercise Solution

--- EmployeeExercise/EmployeeExercise.cpp
+++ EmployeeExercise/EmployeeExercise.cpp
@@ -2,74 +2,26 @@
 #include <iomanip>
 using namespace std;
 
-class clsEmployee {
+class clsPerson {
 private:
 	int _id;
 	string _fName;
 	string _lName;
-	string _title;
-	int _salary;
-	string _department;
 	string _email;
 	string _phone;
 
 public:
-	clsEmployee(int id, string fName, string lName,
-		string email, string title, int salary, 
-		string department, string phone) {
+
+	clsPerson() {
+
+	}
+
+	clsPerson(int id, string fName, string lName, string email, string phone) {
 		_id = id;
 		_fName = fName;
 		_lName = lName;
-		_title = title;
-		_salary = salary;
-		_department = department;
 		_email = email;
 		_phone = phone;
-	}
-
-	// Setters
-	void set_fName(string input) {
-		_fName = input;
-	}
-	void set_lName(string input) {
-		_lName = input;
-	}
-	void set_title(string input) {
-		_title = input;
-	}
-	void set_salary(int input) {
-		_salary = input;
-	}
-	void set_department(string input) {
-		_department = input;
-	}
-	void set_email(string input) {
-		_email = input;
-	}
-	void set_phone(string input) {
-		_phone = input;
-	}
-	// Getters
-	string get_fName() {
-		return _fName;
-	}
-	string get_lName() {
-		return _lName;
-	}
-	string get_title() {
-		return _title;
-	}
-	int get_salary() {
-		return _salary;
-	}
-	string get_department() {
-		return _department;
-	}
-	string get_email() {
-		return _email;
-	}
-	string get_phone() {
-		return _phone;
 	}
 
 	string FullName() {
@@ -82,9 +34,6 @@ public:
 		cout << setw(15) << "First Name : " << _fName << endl;
 		cout << setw(15) << "Last Name : " << _lName << endl;
 		cout << setw(15) << "Full Name : " << FullName() << endl;
-		cout << setw(15) << "Title : " << _title << endl;
-		cout << setw(15) << "Salary : " << _salary << endl;
-		cout << setw(15) << "Departemnt : " << _department << endl;
 		cout << setw(15) << "E-mail : " << _email << endl;
 		cout << setw(15) << "Phone : " << _phone << endl;
 		cout << "_____________________" << endl;
@@ -105,17 +54,75 @@ public:
 		cout << "_____________________" << endl;
 
 	}
+
+	// Setters
+	void set_fName(string input) {
+		_fName = input;
+	}
+	void set_lName(string input) {
+		_lName = input;
+	}
+	void set_email(string input) {
+		_email = input;
+	}
+	void set_phone(string input) {
+		_phone = input;
+	}
+
+	// Getters
+	string get_fName() {
+		return _fName;
+	}
+	string get_lName() {
+		return _lName;
+	}
+	string get_email() {
+		return _email;
+	}
+	string get_phone() {
+		return _phone;
+	}
+};
+
+class clsEmployee : public clsPerson {
+private:
+	string _title;
+	int _salary;
+	string _department;
+
+public:
+	// Setters
+	void set_title(string input) {
+		_title = input;
+	}
+	void set_salary(int input) {
+		_salary = input;
+	}
+	void set_department(string input) {
+		_department = input;
+	}
+
+	// Getters
+	string get_title() {
+		return _title;
+	}
+	int get_salary() {
+		return _salary;
+	}
+	string get_department() {
+		return _department;
+	}
 };
 
 int main()
 {
-	clsEmployee MMM(1,"MMM","Al-Shehri","ialaboudyah@gmail.com","Digital Transformation Section Head",24000,"Digital Transformation","1111111111");
+	clsEmployee Mazin;
 
-	cout << MMM.get_department() << endl;
-	MMM.set_department("IT Governance");
-	cout << MMM.get_department() << endl;
+	Mazin.set_fName("Mazin");
+	Mazin.set_lName("Al-Shehri");
+	Mazin.set_title("Polite Man");
+	Mazin.set_salary(1000000);
 
-	MMM.SendEmail("We Miss The Thursday Meeting", "Honestly, We missed it...");
-
-	MMM.Print();
+	cout << Mazin.get_salary() << endl;
+	Mazin.Print();
 }
